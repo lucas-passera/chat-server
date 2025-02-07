d15be24f763c1a9493a3d69d80839c9ebc682cb4
Lucas Passera <lucasmartinpassera@gmail.com>
2025-02-07 12:50:10 -0300

    fail usernames

--- client/chat-client.py
+++ client/chat-client.py
@@ -1,26 +1,29 @@
 import json
 import websocket
 import threading
+from datetime import datetime
 
-# Evento para sincronizar la conexión y los mensajes recibidos
 message_received_event = threading.Event()
+username = ""
 
 # Función para manejar los eventos de mensaje
 def on_message(ws, message):
-    # Parsear el mensaje recibido (asumiendo que es un JSON)
+
     try:
         msg_data = json.loads(message)
-        # Mostrar solo el contenido del mensaje
-        print(f"Mensaje en el servidor: {msg_data['content']}\n")
+        content = msg_data['content']
+        current_time = datetime.now().strftime("%H:%M:%S")
+        print(f"({current_time}){username}:",content)
     except json.JSONDecodeError:
         print(f"Error al parsear el mensaje: {message}")
     
-    message_received_event.set()  # Notificar que un mensaje ha sido recibido
-# Función para manejar los eventos de conexión
-def on_open(ws):
-    print("Conectado al servidor de chat\n")
-    message_received_event.set()  # Notificar que la conexión está abierta
+    message_received_event.set()
 
+def on_open(ws):
+    global username
+    print(f"\nHi, {username}!")
+
+    message_received_event.set()
 # Función para manejar los eventos de cierre
 def on_close(ws, close_status_code, close_msg):
     print("Conexión cerrada\n")
@@ -30,24 +33,27 @@ def on_error(ws, error):
     print(f"Error: {error}\n")
 
 # Función para enviar mensajes en un hilo separado
-def send_message(ws):
+def send_message(ws, username):
     while True:
-        # Esperar a que un mensaje haya sido recibido antes de pedir otro
-        message_received_event.wait()
-        msg = input("You: ")
+        message_received_event.wait()  # Esperar antes de pedir otro mensaje
+        message_received_event.clear()  # Resetear el evento
+
+        msg = input(f"{username}: ") 
         
         message = {
             "user_id": 1,
             "content": msg
         }
         ws.send(json.dumps(message))
-        print(f"Mensaje enviado!")
         
         # Resetear el evento para esperar la próxima recepción de mensaje
         message_received_event.clear()
 
 # Iniciar la conexión WebSocket y ejecutar en segundo plano
 def start_chat():
+    global username
+    username = input("\nWelcome to chat-server!\n\nIngresa tu nombre de usuario: ")
+
     ws = websocket.WebSocketApp("ws://localhost:8081/chat",
                                 on_message=on_message,
                                 on_error=on_error,
@@ -55,12 +61,11 @@ def start_chat():
     ws.on_open = on_open
 
     # Ejecutar `run_forever` en un hilo separado
-    wst = threading.Thread(target=ws.run_forever)
-    wst.daemon = True
+    wst = threading.Thread(target=ws.run_forever, daemon=True)
     wst.start()
 
     # Iniciar la función de enviar mensajes en el hilo principal
-    send_message(ws)
+    send_message(ws, username)
 
 if __name__ == "__main__":
     start_chat()
