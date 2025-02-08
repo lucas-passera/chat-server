4436908132eabcbcbd9efd0ae755fac9b0a0c9f6
Lucas Passera <lucasmartinpassera@gmail.com>
2025-02-07 23:44:37 -0300

    improving details

--- client/chat-client.py
+++ client/chat-client.py
@@ -2,13 +2,14 @@ import json
 import websocket
 import threading
 from datetime import datetime
+import time
 
 message_received_event = threading.Event()
 
 class ChatClient:
     def __init__(self):
         self.user_id = input("\nWelcome to chat-server!\n\nIngresa tu ID de usuario: ")
-
+        
     def on_message(self, ws, message):
         try:
             msg_data = json.loads(message)
@@ -35,6 +36,9 @@ class ChatClient:
 
     def send_message(self, ws):
         while True:
+            message_received_event.wait()  # Esperar a que el servidor envíe un mensaje primero
+            message_received_event.clear()  # Resetear el evento
+            time.sleep(0.2)
             msg = input(f"{self.user_id}: ")
 
             # Asegurarse de que `user_id` sea un entero
@@ -43,10 +47,7 @@ class ChatClient:
                 "content": msg
             }
             ws.send(json.dumps(message))
-
-            # Esperar la respuesta antes de permitir otro mensaje
-            message_received_event.wait()
-            message_received_event.clear()
+            print(f"{self.user_id}: {msg}\n")
 
     def start_chat(self):
         ws = websocket.WebSocketApp("ws://localhost:8081/chat",
