0c1d2e10198e7e1416bd8f7d658ab52ce53979d2
Federico von Wernich <feddericokz @gmail.com>
2025-02-24 20:11:10 -0300

    Just some comments.

--- client/chat-client.py
+++ client/chat-client.py
@@ -24,6 +24,8 @@ class ChatClient:
         self.password = None
         self.user_data = {"user_id": self.user_id, "username": self.username, "password": self.password}
         self.menu_manager = MenuManager(self)
+
+        ## OWL_TODO: ChatClient's constructor is the one that prints the welcome message and it's the client the owner of the MenuManager?
         self.menu_manager.welcome()
 
 #-------------------------------------------------------------------------------------------------------------------- 
@@ -34,8 +36,8 @@ class ChatClient:
             self.ws = None #reinicia para que no busque en la que estaba, la cual cerro 
 
 #-------------------------------------------------------------------------------------------------------------------- 
-               
-    def on_message(self, ws, message):   
+    ## OWL_TODO: What is ws user for here? It's not being used.
+    def on_message(self, ws, message):
 
         try:
             msg_data = json.loads(message)
@@ -56,20 +58,22 @@ class ChatClient:
         threading.Thread(target=self.send_message, args=(ws,), daemon=True).start()
 
 #--------------------------------------------------------------------------------------------------------------------  
-             
+
+    ## OWL_TODO: What is ws user for here? It's not being used.
     def on_close(self, ws, close_status_code, close_msg):
         print()
         print(Fore.LIGHTRED_EX + "Connection has been closed.\n")
 
 #--------------------------------------------------------------------------------------------------------------------   
-    
+
+    ## OWL_TODO: What is ws user for here? It's not being used.
     def on_error(self, ws, error):
         print(Fore.RED + f"Error: {error}\n")
 
 #--------------------------------------------------------------------------------------------------------------------  
   
     def send_message(self, ws):
-
+        ## OWL_TODO: I don't really understand this, this method is to send a message right? So why does it loop forever?
         while True:
 
             # Waiting for server msg
@@ -105,6 +109,7 @@ class ChatClient:
                                         on_message=self.on_message,
                                         on_error=self.on_error,
                                         on_close=self.on_close)
+        ## OWL_TODO: Can't this be passed in the constructor like the others?
         self.ws.on_open = self.on_open
 
         # create thread for run forever
@@ -112,11 +117,12 @@ class ChatClient:
         wst.start()
         wst.join()
 
+## OWL_TODO: This main could live in some other class right?
 #--------------------------------------------------------------------------------------------------------------------      
 #MAIN   
 if __name__ == "__main__":
 
-    client = ChatClient()  
+    client = ChatClient()
     state_machine = StateMachine(client)
     state_machine.run()
     
